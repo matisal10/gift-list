f783a5569e04683073c235662b2f0a1c999a0ec1
MatiSal <salinasmatias100@gmail.com>
2023-06-22 11:44:41 -0300

    add modal form and input receiver

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from 'react'
 import './index.css'
-import { Button } from '@chakra-ui/react'
+import { Button, useDisclosure } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { Heading } from '@chakra-ui/react'
 import { Highlight } from '@chakra-ui/react'
 import { NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 function App() {
   const initialItems = () => {
@@ -23,8 +33,9 @@ function App() {
   const [gift, setGift] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [link, setLink] = useState('');
+  const [receiver, setReceiver] = useState('');
   const [error, setError] = useState('');
-
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   useEffect(() => {
     if (items.length != 0) {
@@ -57,12 +68,14 @@ function App() {
         setLink('')
         setGift("");
         setQuantity(1);
+        setReceiver('')
         setError("");
       } else {
-        const newItem = { gift: gift, link: link, quantity: quantity };
+        const newItem = { gift: gift, link: link, quantity: quantity, receiver: receiver };
         setItems([...items, newItem]);
         setGift("");
         setLink('')
+        setReceiver('')
         setQuantity(1);
         setError("");
       }
@@ -74,61 +87,74 @@ function App() {
     <>
       <div className="App">
         <div className='container'>
-          <Heading as='h1' size='xl' noOfLines={1}>Regalos</Heading>
-          <form style={{ paddingTop: '10px', display: 'flex' }}>
-            <Input style={{ marginRight: '10px' }} htmlSize={15} width='auto' type="text" value={gift}
-              placeholder='Remera'
-              onChange={(e) => setGift(e.target.value)} />
-            <Input style={{ marginRight: '10px' }} htmlSize={8} width='auto' type="text" value={link}
-              placeholder='https://image...'
-              onChange={(e) => setLink(e.target.value)} />
-            <NumberInput size='md' maxW={24} defaultValue={quantity} min={1} onChange={(_, value) => setQuantity(value)}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <Button colorScheme='orange' style={{ marginLeft: '5px' }} onClick={addGift}>Agregar</Button>
-          </form>
-          {error && (
-            <div style={{ paddingTop: '5px' }}>
-              <Highlight
-                query={error}
-                styles={{ px: '1', py: '1', bg: 'red.100', rounded: 'full' }}
-              >
-                {error}
-              </Highlight>
-            </div>
-          )}
-          <div>
-            {items.length !== 0 ?
-              <ul className='list'>
-                {
-                  items.map((item: any, index: any) => (
-                    <div key={index} className='containerLi'>
-                      <li className='itemList'>
-                        {/* <img src={item.link} width={'40px'} alt="" style={item.link ? { paddingRight: '5px' } : {paddingRight: '5px' ,backgroundColor:'gray'}} /> */}
-                        <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' } } />
-                        {item.gift} (Cantidad: {item.quantity})
-                      </li>
-                      <Button colorScheme='orange' size='xs' onClick={() => deleteItem(index)}>X</Button>
+            <Heading as='h1' size='xl' noOfLines={1}>Regalos:</Heading>
+            <Button onClick={onOpen} style={{ marginTop: '10px' }} colorScheme='orange'>Agregar Regalo</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalCloseButton />
+                <ModalBody>
+                  <form className='form'>
+                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={gift}
+                      placeholder='Remera'
+                      onChange={(e) => setGift(e.target.value)} />
+                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={link}
+                      placeholder='https://image...'
+                      onChange={(e) => setLink(e.target.value)} />
+                    <NumberInput size='md' style={{ marginBottom: '10px' }} maxW='200px' defaultValue={quantity} min={1} onChange={(_, value) => setQuantity(value)}>
+                      <NumberInputField />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={receiver}
+                      placeholder='Destinatario'
+                      onChange={(e) => setReceiver(e.target.value)} />
+                    <Button colorScheme='orange' style={{ marginLeft: '5px' }} onClick={addGift}>Agregar</Button>
+                  </form>
+                  {error && (
+                    <div style={{ paddingTop: '5px' }}>
+                      <Highlight
+                        query={error}
+                        styles={{ px: '1', py: '1', bg: 'red.100', rounded: 'full' }}
+                      >
+                        {error}
+                      </Highlight>
                     </div>
-                  ))
-                }
-              </ul>
-              :
-              <div style={{ padding: '10px' }}>
-                <Highlight query='Agrega algo!' styles={{ px: '1', py: '1', bg: 'orange.100', rounded: 'full' }}>
-                  No hay regalos grinch! Agrega algo!
-                </Highlight>
+                  )}
+                </ModalBody>
+              </ModalContent>
+            </Modal>
+            <div>
+              {items.length !== 0 ?
+                <ul className='list'>
+                  {
+                    items.map((item: any, index: any) => (
+                      <div key={index} className='containerLi'>
+                        <li className='itemList'>
+                          <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' }} />
+                          <div>
+                            <Text fontSize='lg' as='b'>{item.gift}</Text> X{item.quantity}
+                            <Text fontSize='md'>{item.receiver}</Text>
+                          </div>
+                        </li>
+                        <Button colorScheme='orange' size='xs' onClick={() => deleteItem(index)}>X</Button>
+                      </div>
+                    ))
+                  }
+                </ul>
+                :
+                <div style={{ padding: '10px' }}>
+                  <Highlight query='Agrega algo!' styles={{ px: '1', py: '1', bg: 'orange.100', rounded: 'full' }}>
+                    No hay regalos grinch! Agrega algo!
+                  </Highlight>
 
-              </div>
-            }
+                </div>
+              }
+            </div>
+            <Button colorScheme='orange' onClick={deleteAll}>Eliminar Todo</Button>
           </div>
-          <Button colorScheme='orange' onClick={deleteAll}>Eliminar Todo</Button>
-        </div>
-
       </div>
     </>
   )
