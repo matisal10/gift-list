5f52c4caefe1716650d2edf7b1955c336cebbce7
MatiSal <salinasmatias100@gmail.com>
2023-06-23 13:31:32 -0300

    now can edited a gift

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,8 @@ function App() {
   const [receiver, setReceiver] = useState('');
   const [error, setError] = useState('');
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const [edited, setEdited] = useState(false);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
 
   useEffect(() => {
     if (items.length != 0) {
@@ -50,6 +52,34 @@ function App() {
 
   const deleteAll = () => {
     setItems([])
+  }
+
+  const editItem = (item: any, index: any) => {
+    setEdited(true)
+    setGift(item.gift)
+    setLink(item.link)
+    setQuantity(item.quantity)
+    setReceiver(item.receiver)
+    setEditingIndex(index);
+  }
+
+  const editGitf = () => {
+    if (editingIndex !== null) {
+      const updatedItems = [...items];
+      updatedItems[editingIndex] = {
+        gift: gift,
+        link: link,
+        quantity: quantity,
+        receiver: receiver,
+      };
+      setItems(updatedItems);
+      setEdited(false);
+      setGift("");
+      setLink("");
+      setQuantity(1);
+      setReceiver("");
+      setEditingIndex(null); // Re
+    }
   }
 
   const addGift = () => {
@@ -87,74 +117,79 @@ function App() {
     <>
       <div className="App">
         <div className='container'>
-            <Heading as='h1' size='xl' noOfLines={1}>Regalos:</Heading>
-            <Button onClick={onOpen} style={{ marginTop: '10px' }} colorScheme='orange'>Agregar Regalo</Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalCloseButton />
-                <ModalBody>
-                  <form className='form'>
-                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={gift}
-                      placeholder='Remera'
-                      onChange={(e) => setGift(e.target.value)} />
-                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={link}
-                      placeholder='https://image...'
-                      onChange={(e) => setLink(e.target.value)} />
-                    <NumberInput size='md' style={{ marginBottom: '10px' }} maxW='200px' defaultValue={quantity} min={1} onChange={(_, value) => setQuantity(value)}>
-                      <NumberInputField />
-                      <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                      </NumberInputStepper>
-                    </NumberInput>
-                    <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={receiver}
-                      placeholder='Destinatario'
-                      onChange={(e) => setReceiver(e.target.value)} />
-                    <Button colorScheme='orange' style={{ marginLeft: '5px' }} onClick={addGift}>Agregar</Button>
-                  </form>
-                  {error && (
-                    <div style={{ paddingTop: '5px' }}>
-                      <Highlight
-                        query={error}
-                        styles={{ px: '1', py: '1', bg: 'red.100', rounded: 'full' }}
-                      >
-                        {error}
-                      </Highlight>
-                    </div>
-                  )}
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-            <div>
-              {items.length !== 0 ?
-                <ul className='list'>
+          <Heading as='h1' size='xl' noOfLines={1}>Regalos:</Heading>
+          <Button onClick={onOpen} style={{ marginTop: '10px' }} colorScheme='orange'>Agregar Regalo</Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalCloseButton />
+              <ModalBody>
+                <form className='form'>
+                  <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={gift}
+                    placeholder='Remera'
+                    onChange={(e) => setGift(e.target.value)} />
+                  <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={link}
+                    placeholder='https://image...'
+                    onChange={(e) => setLink(e.target.value)} />
+                  <NumberInput size='md' style={{ marginBottom: '10px' }} maxW='200px' defaultValue={quantity} min={1} onChange={(_, value) => setQuantity(value)}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <Input style={{ marginBottom: '10px' }} htmlSize={15} width='auto' type="text" value={receiver}
+                    placeholder='Destinatario'
+                    onChange={(e) => setReceiver(e.target.value)} />
                   {
-                    items.map((item: any, index: any) => (
-                      <div key={index} className='containerLi'>
-                        <li className='itemList'>
-                          <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' }} />
-                          <div>
-                            <Text fontSize='lg' as='b'>{item.gift}</Text> X{item.quantity}
-                            <Text fontSize='md'>{item.receiver}</Text>
-                          </div>
-                        </li>
-                        <Button colorScheme='orange' size='xs' onClick={() => deleteItem(index)}>X</Button>
-                      </div>
-                    ))
+                    edited ? <Button colorScheme='orange' style={{ marginLeft: '5px' }} onClick={()=>{editGitf(),onClose()}}>Editar</Button>
+                      :
+                      <Button colorScheme='orange' style={{ marginLeft: '5px' }} onClick={addGift}>Agregar</Button>
                   }
-                </ul>
-                :
-                <div style={{ padding: '10px' }}>
-                  <Highlight query='Agrega algo!' styles={{ px: '1', py: '1', bg: 'orange.100', rounded: 'full' }}>
-                    No hay regalos grinch! Agrega algo!
-                  </Highlight>
+                </form>
+                {error && (
+                  <div style={{ paddingTop: '5px' }}>
+                    <Highlight
+                      query={error}
+                      styles={{ px: '1', py: '1', bg: 'red.100', rounded: 'full' }}
+                    >
+                      {error}
+                    </Highlight>
+                  </div>
+                )}
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+          <div>
+            {items.length !== 0 ?
+              <ul className='list'>
+                {
+                  items.map((item: any, index: any) => (
+                    <div key={index} className='containerLi'>
+                      <li className='itemList'>
+                        <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' }} />
+                        <div style={{ width: '180px' }}>
+                          <Text fontSize='lg' as='b'>{item.gift}</Text> X{item.quantity}
+                          <Text fontSize='md'>{item.receiver}</Text>
+                        </div>
+                      </li>
+                      <Button colorScheme='orange' size='xs' onClick={() => { editItem(item,index), onOpen() }}>E</Button>
+                      <Button colorScheme='orange' size='xs' onClick={() => deleteItem(index)}>X</Button>
+                    </div>
+                  ))
+                }
+              </ul>
+              :
+              <div style={{ padding: '10px' }}>
+                <Highlight query='Agrega algo!' styles={{ px: '1', py: '1', bg: 'orange.100', rounded: 'full' }}>
+                  No hay regalos grinch! Agrega algo!
+                </Highlight>
 
-                </div>
-              }
-            </div>
-            <Button colorScheme='orange' onClick={deleteAll}>Eliminar Todo</Button>
+              </div>
+            }
           </div>
+          <Button colorScheme='orange' onClick={deleteAll}>Eliminar Todo</Button>
+        </div>
       </div>
     </>
   )
