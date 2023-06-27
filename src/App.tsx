9032536ffef86f4ca4852cf2c80d40bc2c30c7fb
MatiSal <salinasmatias100@gmail.com>
2023-06-27 12:48:02 -0300

    add button duplicated and modal list buy && button print

--- src/App.tsx
+++ src/App.tsx
@@ -73,6 +73,7 @@ function App() {
   const [editingIndex, setEditingIndex] = useState<number | null>(null);
   const [price, setPrice] = useState<number>(0)
   const [total, setTotal] = useState<number>(0)
+  const [isOpenSecondModal, setIsOpenSecondModal] = useState(false);
 
   useEffect(() => {
     if (items.length !== 0) {
@@ -80,6 +81,21 @@ function App() {
       calculateTotal(items); // Calcular el total al actualizar los elementos
     }
   }, [items]);
+
+  useEffect(() => {
+    const hidePrintButton = () => {
+      const printButton = document.querySelector('.print-button');
+      if (printButton) {
+        printButton.style.display = 'none';
+      }
+    };
+  
+    window.addEventListener('beforeprint', hidePrintButton);
+  
+    return () => {
+      window.removeEventListener('beforeprint', hidePrintButton);
+    };
+  }, []);
 
   const deleteItem = (indexToDelete: number) => {
     const updatedItems = items.filter((item, index) => index !== indexToDelete);
@@ -98,6 +114,13 @@ function App() {
     setQuantity(item.quantity)
     setReceiver(item.receiver)
     setEditingIndex(index);
+    setPrice(item.price)
+  }
+  const duplicated = (item: any) => {
+    setGift(item.gift)
+    setLink(item.link)
+    setQuantity(item.quantity)
+    setReceiver(item.receiver)
     setPrice(item.price)
   }
 
@@ -166,9 +189,20 @@ function App() {
 
   const calculateTotal = (total: number) => {
     const newTotal = items.reduce((total: number, item: any) => total + item.quantity * item.price, 0);
-    setTotal(newTotal);
+    const formattedTotal = newTotal.toLocaleString('es-AR');
+    setTotal(formattedTotal);
   }
+  const openSecondModal = () => {
+    setIsOpenSecondModal(true);
+  };
 
+  const closeSecondModal = () => {
+    setIsOpenSecondModal(false);
+  };
+
+  const print = () => {
+    window.print();
+  }
 
   return (
     <>
@@ -225,6 +259,43 @@ function App() {
               </ModalBody>
             </ModalContent>
           </Modal>
+          <Modal isOpen={isOpenSecondModal} onClose={closeSecondModal} size={'xs'}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>
+                <div>
+                  <Heading as='h1' size='xl' noOfLines={1}>Comprar:</Heading>
+                </div>
+              </ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                {items.length !== 0 ?
+                  <ul className='list'>
+                    {
+                      items.map((item: any, index: any) => (
+                        <div key={index} className='listPrev'>
+                          <li className='itemList'>
+                            <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' }} />
+                            <div style={{ width: '180px' }}>
+                              <Text fontSize='lg' as='b'>{item.gift}</Text> x {item.quantity}
+                              <Text fontSize='md'>{item.receiver}</Text>
+                            </div>
+                          </li>
+                        </div>
+                      ))
+                    }
+                    <Button colorScheme='orange' className='print-button' size='sm' onClick={() => print()}>Imprimir</Button>
+                  </ul>
+                  :
+                  <div style={{ padding: '10px' }}>
+                    <Highlight query='Agrega algo!' styles={{ px: '1', py: '1', bg: 'orange.100', rounded: 'full' }}>
+                      No hay regalos grinch! Agrega algo!
+                    </Highlight>
+                  </div>
+                }
+              </ModalBody>
+            </ModalContent>
+          </Modal>
           <div>
             {items.length !== 0 ?
               <ul className='list'>
@@ -234,11 +305,12 @@ function App() {
                       <li className='itemList'>
                         <Image src={item.link} fallbackSrc='https://via.placeholder.com/40' alt='Imagen regalo' boxSize='40px' objectFit='cover' style={{ paddingRight: '5px' }} />
                         <div style={{ width: '180px' }}>
-                          <Text fontSize='lg' as='b'>{item.gift}</Text> X{item.quantity} - ${(item.quantity * item.price)}
+                          <Text fontSize='lg' as='b'>{item.gift}</Text> x {item.quantity} - ${(item.quantity * item.price).toLocaleString('es-AR')}
                           <Text fontSize='md'>{item.receiver}</Text>
                         </div>
                       </li>
                       <Button colorScheme='orange' size='xs' onClick={() => { editItem(item, index), onOpen() }}>E</Button>
+                      <Button colorScheme='orange' size='xs' onClick={() => { duplicated(item), onOpen() }}>D</Button>
                       <Button colorScheme='orange' size='xs' onClick={() => deleteItem(index)}>X</Button>
                     </div>
                   ))
@@ -257,7 +329,8 @@ function App() {
             <Text fontSize='md' as='b'>Total: ${total}</Text>
           </div>
           <div className="line-2" style={{ paddingBottom: '10px' }}></div>
-          <Button colorScheme='orange' onClick={deleteAll}>Eliminar Todo</Button>
+          <Button colorScheme='gray' style={{ marginBottom: '10px' }} onClick={deleteAll}>Eliminar Todo</Button>
+          <Button colorScheme='gray' onClick={openSecondModal}>Previsualizar</Button>
         </div>
       </div>
     </>
